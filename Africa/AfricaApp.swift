0cf267501150d68ee870ef6dce5c1ff77fb518a8
Mitchel Chandara <mmchandara@amarillocollege.com>
2023-05-01 10:53:56 -0500

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by AC Student on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
